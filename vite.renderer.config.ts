287aa5c87793bf4b4788fd2c92f0c113d0efd278
hugtyftg <15224924822@163.com>
2025-10-03 10:35:31 +0800

    feat: format lint

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
 
 // https://vitejs.dev/config
+// 注意是异步加载
 export default defineConfig(async () => {
   const vue = (await import('@vitejs/plugin-vue')).default;
   const tailwindcss = (await import('@tailwindcss/vite')).default;
@@ -10,8 +11,8 @@ export default defineConfig(async () => {
     plugins: [vue(), tailwindcss()],
     resolve: {
       alias: {
-        '@': resolve(__dirname, 'src')
-      }
-    }
-  }
+        '@': resolve(__dirname, 'src'),
+      },
+    },
+  };
 });
